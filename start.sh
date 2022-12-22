c6bf7fa675eaa094d96e67e09d6efd4abef12344
mchridz <55925178+mchridz@users.noreply.github.com>
2022-12-22 14:17:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run -d --restart=always \
-  --name=proxypool \
-  -p 8080:8080 \
-  -v /path/to/config:/toshiki-proxypool-src/config \
-  lukemin/toshiki-proxypool \
+docker run -d --restart=always \ &
+  --name=proxypool \ &
+  -p 8080:8080 \ &
+  -v /path/to/config:/toshiki-proxypool-src/config \ &
+  lukemin/toshiki-proxypool \ &
   -c config/config.yaml
